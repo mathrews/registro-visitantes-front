5ab3a4f64a1d465ca97a3016a825eea4b6f376ae
mathrews <mateusps4gg@gmail.com>
2024-03-14 11:30:43 -0300

    Refatorada função createDataPost da pagina de visitantes e também criada uma função logout no utils para quando o token estiver expirado. toDo: refatorar updateDataPut

--- registro-visitantes-front-tela-visitantes/src/hook/useVisitantes.ts
+++ registro-visitantes-front-tela-visitantes/src/hook/useVisitantes.ts
@@ -1,8 +1,51 @@
-// import { useQuery } from "react-query";
-// import { API } from "../service";
+import { useMutation } from "react-query";
+import { API } from "../service";
+import { logout } from "../utils/logout";
 
-// const config = {
-//     headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
-// };
+const config = {
+    headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+};
 
+type visita = {
+    visitante_id: number;
+    data: string;
+};
 
+type formData = {
+    cpf?: string;
+    nome?: string;
+    dataNascimento?: string;
+    profissao?: string;
+    endereco?: string;
+    numero?: string;
+    complemento?: string;
+    bairro?: string;
+    cidade?: string;
+    uf?: string;
+    cep?: string;
+    genero_id?: number;
+};
+
+export const useVisitaCreate = () => {
+    return useMutation(async (data: visita) => {
+        const visitaResponse = await API.post("/visita", data, config);
+        if (visitaResponse.data == "Token expirado!") {
+            logout();
+            window.location.reload();
+        } else {
+            return visitaResponse.data;
+        }
+    });
+};
+
+export const useVisitanteCreate = () => {
+    return useMutation(async (data: formData) => {
+        const response = await API.post("/visitante", data, config);
+        if (response.data == "Token expirado!") {
+            logout();
+            window.location.reload();
+        } else {
+            return response.data;
+        }
+    });
+};
